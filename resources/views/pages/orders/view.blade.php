86713bd78e275dd60652e4bd7592099d3056765c
aburaihan8989 <yusup.smartphone@gmail.com>
2025-03-10 10:42:47 +0700

    update

--- resources/views/pages/orders/view.blade.php
+++ resources/views/pages/orders/view.blade.php
@@ -52,7 +52,7 @@
                             <div class="col-sm-4">Grand Total</div>
                             <div>: Rp. {{ number_format(($order->total_price), 0, ",", ".") }}</div>
                         </div>
-                        <div class="form-group row">
+                        <div class="form-group row mb-1">
                             <div class="col-sm-4">Total Item</div>
                             <div>: {{ $order->total_item }}</div>
                         </div>
